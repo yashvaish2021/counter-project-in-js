cb6aba6c74e1cb5de56f99edd47247dc618d1db5
Yash Vaish <yashvaish2021@gmail.com>
2022-09-01 10:31:21 +0530

    Changed the color of the values

--- app.js
+++ app.js
@@ -6,15 +6,27 @@ let currentCountValue = 0;
 
 increaseBtn.addEventListener("click", function () {
   currentCountValue++;
-  counterValue.textContent = currentCountValue;
+  // counterValue.textContent = currentCountValue;
+  giveColor();
 });
 
 resetBtn.addEventListener("click", function () {
   currentCountValue = 0;
-  counterValue.textContent = 0;
+  // counterValue.textContent = 0;
+  giveColor();
 });
 
 decreaseBtn.addEventListener("click", function () {
   currentCountValue--;
-  counterValue.textContent = currentCountValue;
+  // counterValue.textContent = currentCountValue;
+  giveColor();
 });
+
+function giveColor() {
+  counterValue.textContent = currentCountValue;
+  if (currentCountValue < 0) {
+    counterValue.style.color = "red";
+  } else if (currentCountValue > 0) {
+    counterValue.style.color = "green";
+  } else counterValue.style.color = "black";
+}
